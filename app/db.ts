e339f055d4b29b20789591c9f7a4dd85364f6d28
Gamer Snave <snaveqiptoo@gmail.com>
2024-09-01 23:16:58 +0300

    Updated Database

--- app/db.ts
+++ app/db.ts
@@ -1,12 +1,9 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
-import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, serial, varchar, boolean } from 'drizzle-orm/pg-core';
 import { eq } from 'drizzle-orm';
 import postgres from 'postgres';
 import { genSaltSync, hashSync } from 'bcrypt-ts';
 
-// Optionally, if not using email/pass login, you can
-// use the Drizzle adapter for Auth.js / NextAuth
-// https://authjs.dev/reference/adapter/drizzle
 let client = postgres(`${process.env.POSTGRES_URL!}?sslmode=require`);
 let db = drizzle(client);
 
@@ -15,12 +12,12 @@ export async function getUser(email: string) {
   return await db.select().from(users).where(eq(users.email, email));
 }
 
-export async function createUser(email: string, password: string) {
+export async function createUser(name: string, email: string, password: string, language: string, sayName: boolean) {
   const users = await ensureTableExists();
   let salt = genSaltSync(10);
   let hash = hashSync(password, salt);
 
-  return await db.insert(users).values({ email, password: hash });
+  return await db.insert(users).values({ name, email, password: hash, language, say_name: sayName });
 }
 
 async function ensureTableExists() {
@@ -35,15 +32,21 @@ async function ensureTableExists() {
     await client`
       CREATE TABLE "User" (
         id SERIAL PRIMARY KEY,
+        name VARCHAR(64),
         email VARCHAR(64),
-        password VARCHAR(64)
+        password VARCHAR(64),
+        language VARCHAR(64),
+        say_name BOOLEAN
       );`;
   }
 
   const table = pgTable('User', {
     id: serial('id').primaryKey(),
+    name: varchar('name', { length: 64 }),
     email: varchar('email', { length: 64 }),
     password: varchar('password', { length: 64 }),
+    language: varchar('language', { length: 64 }),
+    say_name: boolean('say_name'),
   });
 
   return table;
