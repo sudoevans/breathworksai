201f0059014fcfe4945905fc57bc415f60d7bc2a
Solemn <davidechoda123@gmail.com>
2024-09-10 20:08:20 +0100

    breathing guidance

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,18 +1,7 @@
-'use client'
-import React, { ReactNode, useEffect } from 'react'
+import React, { ReactNode } from 'react'
 import Logo from '../components/Logo'
-import { usePathname, useRouter } from 'next/navigation';
 
 const layout = ({children}: {children: ReactNode}) => {
-  const router = useRouter();
-  const path = usePathname()
-
-  useEffect(() => {
-    // Redirect only if the current route is /home
-    if (path === '/home') {
-      router.replace('/home/create');
-    }
-  }, [router]);
   return (
     <div className="min-h-screen text-white pt-4 pb-[5rem] flex flex-col items-center relative overflow-hidden">
       <div className="z-10 w-full max-w-xl px-4 text-center">
